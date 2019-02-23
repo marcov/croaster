ad27a85a26a299157be9c8191c535af991eeed5b
Marco Vedovati <mvedovati@suse.com>
2019-02-23 20:08:02 +0100

    add bits 2 bytes macro

--- src/misc.h
+++ src/misc.h
@@ -2,16 +2,18 @@
 #define __MISC_H__
 
 
-#define ASSERT(condition) do { if (condition) {} } while (0);
-
-
 extern char printfBuf[32];
+
+////////////////////////////////////////////////////////////////////////////////
+
+#define ASSERT(condition) do { if (condition) {} } while (0);
 
 #define PRINTF(...)  do { \
   snprintf(printfBuf, sizeof(printfBuf), __VA_ARGS__); \
   Serial.print(printfBuf); \
 } while (0)
 
-
+#define BITS_TO_BYTES(bits) \
+    ((bits) / 8 + (((bits) % 8) ? 1 : 0))
 
 #endif /* #ifndef __MISC_H__ */
